d3dec290b7eb0ec7be05c261c30d949c326fa38f
András Kornél Nagy <n.kornel2002@gmail.com>
2026-05-03 17:43:10 +0200

    application.yml config file

--- backend/src/main/java/application/Application.java
+++ backend/src/main/java/application/Application.java
@@ -1,7 +1,9 @@
 package application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
     static void main(String[] args) {
         SpringApplication.run(Application.class, args);
